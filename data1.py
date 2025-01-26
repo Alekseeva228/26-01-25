64a84752db3a54b623926fa8509e74fa1c19b9e0
Alekseeva228 <tumonya79@gmail.com>
2025-01-26 17:23:07 +0700

    Add files via upload

--- data1.py
+++ data1.py
@@ -1,5 +1,9 @@
 file=open('data.txt','w',encoding="utf-8")
 text=input("Введите текст:")
-file1=open('data.txt','w',encoding="utf-8")
 file.write(text)
+text1=input("Введите текст:")
+file.write(text1)
+text2=input("Введите текст:")
+file.write(text2)
 file.close()
+
